50e27ade022d1c9a14e384313cf2a42f143346cf
Firat Göktepe <firat@naxnordic.com>
2021-09-23 20:00:11 +0300

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,19 @@
 import Head from 'next/head'
 import Image from 'next/image'
+
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql
+} from "@apollo/client";
+
+
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home({posts}) {
+  console.log('posts', posts)
   return (
     <div className={styles.container}>
       <Head>
@@ -22,33 +33,15 @@ export default function Home() {
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          {posts.map(post => {
+            return (
+              <a key={post.title} href="#" className={styles.card}>
+                <h2>{post.title}</h2>
+                <div dangerouslySetInnerHTML={{__html: `<p>${post.excerpt}</p>`}}></div>
+              </a>
+            )
+          })}
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
         </div>
       </main>
 
@@ -66,4 +59,38 @@ export default function Home() {
       </footer>
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const client = new ApolloClient({
+    uri: process.env.WORDPRESS_GRAPHQL_ENDPOINT,
+    cache: new InMemoryCache()
+  });
+
+  const response = await client.query({
+    query: gql`
+      query MyQuery {
+        posts {
+          edges {
+            node {
+              title
+              uri
+              excerpt
+              content
+            }
+          }
+        }
+      }
+    `
+  });
+
+  const posts = response.data.posts.edges.map(({ node }) => node);
+
+  console.log('posts', posts)
+
+  return {
+    props: {
+      posts
+    }
+  }
 }
